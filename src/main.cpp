64531367e67aaeeea9cb6438e0328b42f2ac96ea
Shahtr1 <tramboos123@gmail.com>
2025-01-18 15:50:50 +0530

    tokens_to_asm refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -4,111 +4,52 @@
 #include <sstream>
 #include <vector>
 
-enum class TokenType
-{
-  _return,
-  int_literal,
-  semicolon,
-};
-
-struct Token
-{
-  TokenType type;
-  std::optional<std::string> value;
-};
-
-std::vector<Token> tokenize(const std::string& str){
-  std::vector<Token> tokens;
-  std::string buf;
-
-  for (int i = 0; i < str.length(); i++) {
-    char c = str[i];
-    if (std::isalpha(c)) {
-      buf.push_back(c);
-      i++;
-      while (std::isalnum(str[i])) {
-        buf.push_back(str[i]);
-        i++;
-      }
-      i--;
-
-      if (buf == "return") {
-        tokens.push_back({.type = TokenType::_return});
-        buf.clear();
-        continue;
-      }
-      std::cerr << "You messed up!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    if (std::isdigit(c)) {
-      buf.push_back(c);
-      i++;
-      while (std::isdigit(str[i])) {
-        buf.push_back(str[i]);
-        i++;
-      }
-      i--;
-      tokens.push_back({.type = TokenType::int_literal, .value = buf});
-      buf.clear();
-    }
-    else if (c == ';') {
-      tokens.push_back({.type = TokenType::semicolon});
-    }
-    else if (std::isspace(c)) {}
-    else {
-      std::cerr << "You messed up!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  return tokens;
-}
+#include "./tokenization.h"
 
 std::string tokens_to_asm(const std::vector<Token>& tokens){
-  std::stringstream output;
-  output << "global _start\n_start:\n";
-  for (int i = 0; i < tokens.size(); i++) {
-    const Token& token = tokens[i];
-    if (token.type == TokenType::_return) {
-      if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::int_literal) {
-        if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::semicolon) {
-          output << "    mov rax, 60\n";
-          output << "    mov rdi, " << tokens[i + 1].value.value() << "\n";
-          output << "    syscall";
+    std::stringstream output;
+    output << "global _start\n_start:\n";
+    for (int i = 0; i < tokens.size(); i++) {
+        const Token& token = tokens[i];
+        if (token.type == TokenType::exit) {
+            if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::int_literal) {
+                if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::semicolon) {
+                    output << "    mov rax, 60\n";
+                    output << "    mov rdi, " << tokens[i + 1].value.value() << "\n";
+                    output << "    syscall";
+                }
+            }
         }
-      }
     }
-  }
 
-  return output.str();
+    return output.str();
 }
 
 int main(int argc, char* argv[]){
-  if (argc != 2) {
-    std::cerr << "Incorrect Usage: " << std::endl;
-    std::cerr << "Usage: hydro <input.hy>" << std::endl;
-    return EXIT_FAILURE;
-  }
+    if (argc != 2) {
+        std::cerr << "Incorrect Usage: " << std::endl;
+        std::cerr << "Usage: hydro <input.hy>" << std::endl;
+        return EXIT_FAILURE;
+    }
 
+    std::string contents;
 
-  std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
 
-  {
-    std::stringstream contents_stream;
-    std::fstream input(argv[1], std::ios::in);
-    contents_stream << input.rdbuf();
-    contents = contents_stream.str();
-  }
+    {
+        Tokenizer tokenizer(std::move(contents));
+        std::vector<Token> tokens = tokenizer.tokenize();
+        std::fstream file("out.asm", std::ios::out);
+        file << tokens_to_asm(tokens);
+    }
 
-  {
-    std::vector<Token> tokens = tokenize(contents);
-    std::fstream file("out.asm", std::ios::out);
-    file << tokens_to_asm(tokens);
-  }
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
 
-  system("nasm -felf64 out.asm");
-  system("ld -o out out.o");
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
